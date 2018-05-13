bddc568179a07b0567108f0db2fdd33044fb8b14
Hiep Trinh <hieptq.social@gmail.com>
2018-05-13 15:51:39 +1000

    tru

--- Source/configs/keys.js
+++ Source/configs/keys.js
@@ -2,4 +2,5 @@ module.exports = {
     // mongoURI: 'mongodb+srv://swindeveloper:PKvWbwnH2Vq59VB3@socialnetwork-upnch.mongodb.net/test?retryWrites=true'
     // url: 'mongodb://swindeveloper:PKvWbwnH2Vq59VB3@socialnetwork-shard-00-00-upnch.mongodb.net:27017,socialnetwork-shard-00-01-upnch.mongodb.net:27017,socialnetwork-shard-00-02-upnch.mongodb.net:27017/test?ssl=true&replicaSet=SocialNetwork-shard-0&authSource=admin&retryWrites=true'
     url: 'mongodb://hiepmikedb:qwer1234@ds121950.mlab.com:21950/twittertrenddb'
+    ggggggg
 }
